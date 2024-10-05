e00124e47b1143c79785f20c7ff1ef2a1fe21140
makber29 <makber2950@gmail.com>
2024-10-05 14:04:41 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  testMatch: [
-    "**/__tests__/**/*.js",
-    "**/?(*.)+(spec|test|cy).[jt]s?(x)"
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/"
-  ],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
