3a210274855c053ba5a15ead93fbc075ae93a199
Syed Hassaan Zafar <syedhassaanzfr@gmail.com>
2024-10-05 15:53:25 +0500

    updated file addreport supports

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,19 +1,48 @@
-const { defineConfig } = require("cypress");
-require('dotenv').config()
+// const { defineConfig } = require("cypress");
+// require('dotenv').config()
+
+// module.exports = defineConfig({
+
+//   // viewportWidth: 375,
+//   // viewportHeight: 812,
+//   numTestsKeptInMemory: 10,
+//   experimentalMemoryManagement: true,
+//   defaultCommandTimeout: 30000,
+//   video: true, // Enable video recording
+//   screenshotsFolder: 'cypress/screenshots', // Default path for screenshots
+//   videosFolder: 'cypress/videos',
+
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//     },
+//   },
+// });
+
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
-  // viewportWidth: 375,
-  // viewportHeight: 812,
   numTestsKeptInMemory: 10,
   experimentalMemoryManagement: true,
   defaultCommandTimeout: 30000,
   video: true, // Enable video recording
-  screenshotsFolder: 'cypress/screenshots', // Default path for screenshots
+  screenshotsFolder: 'cypress/screenshots',
   videosFolder: 'cypress/videos',
+  reporter: 'cypress-allure-plugin', // Use Allure reporter
 
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config); // Enable Allure plugin
+      return config;
+    },
+  },
+
+  reporterOptions: {
+    allure: {
+      outputDir: 'cypress/reports/allure-results', // Allure results output directory
+      overwrite: false,
+      clean: true,
     },
   },
 });
