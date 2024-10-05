be1fee918abab8c55e9c2ffa7495d4ee667dc0c7
Syed Hassaan Zafar <syedhassaanzfr@gmail.com>
2024-10-05 15:07:19 +0500

    updated 2 files

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -8,6 +8,9 @@ module.exports = defineConfig({
   numTestsKeptInMemory: 10,
   experimentalMemoryManagement: true,
   defaultCommandTimeout: 30000,
+  video: true, // Enable video recording
+  screenshotsFolder: 'cypress/screenshots', // Default path for screenshots
+  videosFolder: 'cypress/videos',
 
   e2e: {
     setupNodeEvents(on, config) {
