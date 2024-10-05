54f9b62d31a1200f7a238585913a31a44aeebebf
Syed Hassaan Zafar <syedhassaanzfr@gmail.com>
2024-10-05 13:58:18 +0500

    updated yaml and config fike

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  testMatch: [
+    "**/__tests__/**/*.js",
+    "**/?(*.)+(spec|test|cy).[jt]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/"
+  ],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
