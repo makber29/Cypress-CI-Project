6f6fa75249380f042ec5a23e192694b763382d9e
makber <syedhassaanzfr@gmail.com>
2024-10-08 20:24:08 +0500

    testttt

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -18,41 +18,30 @@
 //   },
 // });
 
-// const { defineConfig } = require('cypress');
-// const { json } = require('react-router-dom');
+const { defineConfig } = require('cypress');
+const { json } = require('react-router-dom');
 
-// module.exports = defineConfig({
+module.exports = defineConfig({
 
-//   numTestsKeptInMemory: 10,
-//   experimentalMemoryManagement: true,
-//   defaultCommandTimeout: 30000,
-//   video: true, // Enable video recording
-//   screenshotsFolder: 'cypress/screenshots', // Default path for screenshots
-//   videosFolder: 'cypress/videos', // Default path for videos
-//   reporter: 'mochawesome', // Specify the reporter
-//   reporterOptions: {
-//     reportDir: 'cypress/reports', // Folder for saving reports
-//     overwrite: false,
-//     html: true,
-//     json:true,
-//     merge: true,
-//   },
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // Add node event listeners here if needed
-//     },
-//   },
-// });
-
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-
-module.exports = {
+  numTestsKeptInMemory: 10,
+  experimentalMemoryManagement: true,
+  defaultCommandTimeout: 30000,
+  video: true, // Enable video recording
+  screenshotsFolder: 'cypress/screenshots', // Default path for screenshots
+  videosFolder: 'cypress/videos', // Default path for videos
+  reporter: 'mochawesome', // Specify the reporter
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Folder for saving reports
+    overwrite: false,
+    html: true,
+    json:true,
+    merge: true,
+    embeddedScreenshots: true,  // Embed screenshots in the report
+    inlineAssets: true,  // Show screenshots directly in the HTML report
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);  // Setup Allure plugin
-      return config;
+      // Add node event listeners here if needed
     },
   },
-  video: true,  // Enable video recording
-  screenshotOnRunFailure: true,  // Take screenshots on test failure
-};
+});
